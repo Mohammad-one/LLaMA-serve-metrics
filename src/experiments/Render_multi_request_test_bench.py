4a4b67b85b553f0b9798832236870ee5e6d3f2f5
Mohammad-one <mohammadghanbari.one@yahoo.com>
2025-07-19 16:38:36 +0330

    fix: fix metrix miscalculation

--- src/experiments/Render_multi_request_test_bench.py
+++ src/experiments/Render_multi_request_test_bench.py
@@ -9,14 +9,14 @@ from src.experiments.models.enums import BenchmarkColumns, ContentType
 from transformers import AutoTokenizer
 
 CLIENT_COUNTS = [2]
-PROMPT_LENGTHS = [10]
+PROMPT_LENGTHS = [200]
 CONTENT_TYPE = ContentType.RENDER
-MAX_TOKENS = 1
-BASE_DIR = r"C:\Users\ASUS\Desktop\Pars\HardwareAware\src\experiments\data\rendering"
+MAX_TOKENS = 100
+BASE_DIR = r"C:\Users\ASUS\Desktop\Pars\HardwareAware\src\experiments\data\Llama_cpp_Ashkan_final"
 
 client = openai.OpenAI(
-    #base_url="http://192.168.88.130:8080",
-     base_url="http://192.168.70.124:5000/v1",
+    base_url="http://192.168.88.130:8080",
+    # base_url="http://192.168.70.124:5000/v1",
     api_key="sk-no-key-required"
 )
 
@@ -35,7 +35,7 @@ def init_csv_file(client_count, prompt_length):
         os.makedirs(directory)
 
     if not os.path.exists(csv_path) or os.stat(csv_path).st_size == 0:
-        with open(csv_path, 'w', newline='') as csvfile:
+        with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=COLUMNS)
             writer.writeheader()
 
@@ -45,9 +45,6 @@ def init_csv_file(client_count, prompt_length):
 def save_to_csv(metrics, csv_path):
     saved_metrics = {k: v for k, v in metrics.items() if k in COLUMNS}
 
-    for col_name, value in saved_metrics.items():
-        if value is not None and isinstance(value, datetime):
-            saved_metrics[col_name] = value.strftime('%Y-%m-%d %H:%M:%S.%f')
 
     with open(csv_path, 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=COLUMNS)
@@ -67,6 +64,8 @@ def send_request(session_id, csv_path, prompt):
         'BT': time.time(),
         'FT': None,
         'LT': None,
+        'TGT': None,
+        'completion_tokens': None,
         'prompt_tokens': None,
         'prompt_ms': None,
         'prompt_per_token_ms': None,
@@ -79,6 +78,7 @@ def send_request(session_id, csv_path, prompt):
     }
 
     try:
+        metrics['BT'] = time.time()
         stream = client.chat.completions.create(
             # model="Qwen/Qwen3-0.6B", #peyman
             model="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
@@ -101,6 +101,8 @@ def send_request(session_id, csv_path, prompt):
 
                     metrics.update({
                         'prompt_tokens': usage_data.prompt_tokens if hasattr(usage_data, 'prompt_tokens') else None,
+                        'completion_tokens': usage_data.completion_tokens if hasattr(usage_data,
+                                                                                 'completion_tokens') else None,
                         'prompt_ms': timings_data.get('prompt_ms'),
                         'prompt_per_token_ms': timings_data.get('prompt_per_token_ms'),
                         'prompt_per_second': timings_data.get('prompt_per_second'),
@@ -118,7 +120,10 @@ def send_request(session_id, csv_path, prompt):
         if metrics['LT'] and metrics['FT']:
             metrics['TGT'] = (metrics['LT'] - metrics['FT']) * 1000
         if metrics.get('TTFT') and metrics['prompt_tokens']:
-            metrics['PP'] = metrics['prompt_tokens'] / ((metrics['TTFT'])/1000)
+            metrics['PP'] = metrics['prompt_tokens'] / ((metrics['TTFT']) / 1000)
+        if metrics.get('TGT') and metrics['completion_tokens']:
+            metrics['TG'] = metrics['completion_tokens'] / (metrics['TGT'] / 1000)
+
 
     except Exception as e:
         metrics['error'] = str(e)
@@ -126,8 +131,6 @@ def send_request(session_id, csv_path, prompt):
     for time_field in ['BT', 'FT', 'LT']:
         if metrics[time_field] is not None:
             metrics[time_field] = datetime.fromtimestamp(metrics[time_field])
-
-    metrics['content_sample'] = (metrics['content'][:100]) if metrics['content'] else None
 
     save_to_csv(metrics, csv_path)
     return metrics
